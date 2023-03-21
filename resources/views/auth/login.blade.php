5d93b0d62dda92a994e35097cf7db4c1ead62c6b
danixsofyan <danixsofyan@gmail.com>
2023-03-21 14:44:19 +0700

    update logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,22 +7,36 @@
         <x-validation-errors class="mb-4" />
 
         @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
-                {{ session('status') }}
-            </div>
+        <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
+            {{ session('status') }}
+        </div>
         @endif
 
         <form method="POST" action="{{ route('login') }}">
             @csrf
 
             <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                <h2 class="mt-6 text-xl font-semibold leading-tight text-center text-gray-800 dark:text-gray-400">
+                    Sign in to your account
+                </h2>
+                <p class="mt-2 text-sm text-center text-gray-600 dark:text-white">
+                    Or
+                    <a class="text-sm text-center text-gray-600 underline hover:text-gray-900 dark:text-white dark:hover:text-gray-400"
+                        href="{{ route('register') }}">
+                        {{ __('Create an Account') }}
+                    </a>
+                </p>
+            </div>
+            <div>
+                <x-label for="email" value="{{ __('Email') }}" class="mt-6" />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                    autofocus autocomplete="username" />
             </div>
 
             <div class="mt-4">
                 <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                    autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
@@ -34,15 +48,17 @@
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                    href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
                 @endif
 
                 <x-button class="ml-4">
                     {{ __('Log in') }}
                 </x-button>
             </div>
+
         </form>
     </x-authentication-card>
 </x-guest-layout>
